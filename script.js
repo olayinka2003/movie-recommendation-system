dab3a43a9b34e42097db9f31520f6d59299da263
olayinka <olayinkaolodude5@gmail.com>
2024-05-12 13:21:43 +0100

    display the movies

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const apiKey = "api_key=c7cba54e8036127f06f9792f9781dbad";
 const baseUrl = "https://api.themoviedb.org/3";
 const apiURl = baseUrl + "/discover/movie?sort_by=popularity.desc&" + apiKey;
+const imgUrl = "https://image.tmdb.org/t/p/w500";
+const main = document.getElementById("main");
 
 function getMovies(url) {
   fetch(url)
@@ -13,20 +15,30 @@ function getMovies(url) {
 getMovies(apiURl);
 
 function showMovies(data) {
+  main.innerHTML = "";
   data.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
     const movieDiv = document.createElement("div");
     movieDiv.classList.add("movie");
-    movieDiv.innerHTML = ` <div class="movie">
-    <img src="img/action.jpg" alt="action logo" />
+    movieDiv.innerHTML = `<img src="${imgUrl + poster_path}" alt="${title}" />
     <div class="movie-info">
-      <h3>Movie Title</h3>
-      <span class="green">9.8</span>
+      <h3>${title}</h3>
+      <span class="${getColor(vote_average)}">${vote_average}</span>
     </div>
     <div class="overview">
       <h3>Overview</h3>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex, adipisci
-      suscipit quia, eius error doloribus minus eveniet,
-    </div>
-  </div>`;
+         ${overview}
+    </div>`;
+    main.appendChild(movieDiv); // Move this line inside the forEach loop
   });
+}
+
+function getColor(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
 }
