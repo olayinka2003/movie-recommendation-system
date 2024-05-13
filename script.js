7891d1c99d5d46617a7b544a718f87fcb2f1070c
olayinka <olayinkaolodude5@gmail.com>
2024-05-13 22:42:43 +0100

    filter

--- script.js
+++ script.js
@@ -6,6 +6,101 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 const searchURl = baseUrl + "/search/movie?" + apiKey;
+const genre = {
+  genres: [
+    {
+      id: 28,
+      name: "Action",
+    },
+    {
+      id: 12,
+      name: "Adventure",
+    },
+    {
+      id: 16,
+      name: "Animation",
+    },
+    {
+      id: 35,
+      name: "Comedy",
+    },
+    {
+      id: 80,
+      name: "Crime",
+    },
+    {
+      id: 99,
+      name: "Documentary",
+    },
+    {
+      id: 18,
+      name: "Drama",
+    },
+    {
+      id: 10751,
+      name: "Family",
+    },
+    {
+      id: 14,
+      name: "Fantasy",
+    },
+    {
+      id: 36,
+      name: "History",
+    },
+    {
+      id: 27,
+      name: "Horror",
+    },
+    {
+      id: 10402,
+      name: "Music",
+    },
+    {
+      id: 9648,
+      name: "Mystery",
+    },
+    {
+      id: 10749,
+      name: "Romance",
+    },
+    {
+      id: 878,
+      name: "Science Fiction",
+    },
+    {
+      id: 10770,
+      name: "TV Movie",
+    },
+    {
+      id: 53,
+      name: "Thriller",
+    },
+    {
+      id: 10752,
+      name: "War",
+    },
+    {
+      id: 37,
+      name: "Western",
+    },
+  ],
+};
+const tagEl = document.getElementById("tag");
+setGenre();
+function setGenre() {
+  tagEl.innerHTML = "";
+  genre.genres.forEach((genre) => {
+    const t = document.createElement("div");
+    t.classList.add("tags");
+    t.id = genre.id;
+    t.innerText = genre.name;
+    t.addEventListener("click", (e) => {
+      getMovies(apiURl + "&with_genres=" + e.target.id);
+    });
+    tagEl.appendChild(t);
+  });
+}
 
 function getMovies(url) {
   fetch(url)
@@ -32,7 +127,7 @@ function showMovies(data) {
       <h3>Overview</h3>
          ${overview}
     </div>`;
-    main.appendChild(movieDiv); // Move this line inside the forEach loop
+    main.appendChild(movieDiv);
   });
 }
 
