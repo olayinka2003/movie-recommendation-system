0e4b03177a51efc27a97803fe336ecc3b1434637
olayinka <olayinkaolodude5@gmail.com>
2024-05-18 03:54:29 +0100

    few changes

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ getMovies(apiURl);
 function showMovies(data) {
   main.innerHTML = "";
   data.forEach((movie) => {
-    const { title, poster_path, vote_average, overview } = movie;
+    const { title, poster_path, vote_average, overview, id } = movie;
     const movieDiv = document.createElement("div");
     movieDiv.classList.add("movie");
     movieDiv.innerHTML = `<img src="${imgUrl + poster_path}" alt="${title}" />
@@ -127,10 +127,86 @@ function showMovies(data) {
     <div class="overview">
       <h3>Overview</h3>
          ${overview}
+         <br/>
+         <button class= "Know-more" id="${id}" >Know more</button>
     </div>`;
     main.appendChild(movieDiv);
+
+    document.getElementById(id).addEventListener("click", () => {
+      console.log(id);
+      openNav(movie);
+    });
   });
 }
+const overlay = document.getElementById("overlay-content");
+/* Open when someone clicks on the span element */
+function openNav(movie) {
+  let id = movie.id;
+  fetch(baseUrl + `/movie/` + id + `/videos?` + apiKey)
+    .then((response) => response.json())
+    .then((videoData) => {
+      console.log(videoData);
+      if (videoData) {
+        document.getElementById("myNav").style.width = "100%";
+        if (videoData.results.length > 0) {
+          var embed = [];
+          videoData.results.forEach((video) => {
+            let { name, key, site } = video;
+            if (site == "YouTube") {
+              embed.push(`
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/${key}?si=2KP-XLKD2ik0WFJ7" title="${name}" class = "embed hide"frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              
+              `);
+            }
+          });
+
+          overlay.innerHTML = embed.join("");
+          activeSlide = 0;
+          showVideos();
+        }
+      }
+    });
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+var activeSlide = 0;
+var totalVideos = 0;
+function showVideos() {
+  let embedclasses = document.querySelectorAll(".embed");
+  totalVideos = embedclasses.length;
+  embedclasses.forEach((embedclass, idx) => {
+    if (activeSlide == idx) {
+      embedclass.classList.add("show");
+      embedclass.classList.remove("hide");
+    } else {
+      embedclass.classList.add("hide");
+      embedclass.classList.remove("show");
+    }
+  });
+}
+
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+
+leftArrow.addEventListener("click", () => {
+  if (activeSlide > 0) {
+    activeSlide--;
+  } else {
+    activeSlide = totalVideos - 1;
+  }
+  showVideos();
+});
+rightArrow.addEventListener("click", () => {
+  if (activeSlide < totalVideos - 1) {
+    activeSlide++;
+  } else {
+    activeSlide = 0;
+  }
+  showVideos();
+});
 
 function getColor(vote) {
   if (vote >= 8) {
